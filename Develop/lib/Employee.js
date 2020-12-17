42c8f6c50f5b1365e98345eb94262ab260f36dd1
christiansuchoski <christiansuchoski@gmail.com>
2020-12-17 12:21:01 -0800

    added code to app.js file

---
+++ Develop/lib/Employee.js
@@ -0,0 +1,26 @@
+
+// TODO: Write code to define and export the Employee class
+class Employee {
+    constructor(name,id,email) {
+      this.name = name;
+      this.id = id;
+      this.email = email;
+      this.role = "";
+    }
+  
+    getId(){
+      return this.id;
+    }
+    getName(){
+        return this.name;
+    }
+    getEmail(){
+        return this.email;
+    }
+    getRole(){
+        this.role = "Employee";
+        return this.role;
+    }
+  }
+  
+  module.exports = Employee;
